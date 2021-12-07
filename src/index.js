61268a1d59c7f093dd663b552dbaa1b87bc8c372
Maxim <shafeevms@gmail.com>
2021-12-07 21:10:06 +0300

    header css started

--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
 const citySelect = document.querySelector('.header__select');
+const categorySelect = document.querySelector('.header__categSelect');
 
-const choices = new Choices(citySelect, {
-  silent: false,
-  items: [],
-  choices: [],
-  renderChoiceLimit: -1,
-  maxItemCount: -1,
-  addItems: true,
-  addItemFilter: null,
-  removeItems: true,
-  removeItemButton: false,
-  editItems: false,
-  duplicateItemsAllowed: true,
-  delimiter: ',',
-  paste: true,
-  searchEnabled: true,
-  searchChoices: true,
-  searchFloor: 1,
-  searchResultLimit: 4,
+const cityChoices = new Choices(citySelect, {
+  searchEnabled: false,
   searchFields: ['label', 'value'],
-  position: 'auto',
-  resetScrollPosition: true,
-  shouldSort: true,
-  shouldSortItems: false,
-  // sorter: () => {...},
-  placeholder: true,
-  placeholderValue: null,
-  searchPlaceholderValue: null,
-  prependValue: null,
-  appendValue: null,
-  renderSelectedChoices: 'auto',
-  loadingText: 'Loading...',
-  noResultsText: 'No results found',
-  noChoicesText: 'No choices to choose from',
-  itemSelectText: 'Press to select',
-  addItemText: (value) => {
-    return `Press Enter to add <b>"${value}"</b>`;
-  },
-  maxItemText: (maxItemCount) => {
-    return `Only ${maxItemCount} values can be added`;
-  },
-  valueComparer: (value1, value2) => {
-    return value1 === value2;
-  },
+  itemSelectText: '',
   classNames: {
     containerOuter: 'choices',
     containerInner: 'choices__inner',
@@ -73,9 +35,44 @@ const choices = new Choices(citySelect, {
   },
   // Choices uses the great Fuse library for searching. You
   // can find more options here: https://github.com/krisk/Fuse#options
-  fuseOptions: {
-    include: 'score'
-  },
-  callbackOnInit: null,
-  callbackOnCreateTemplates: null
+  // fuseOptions: {
+  //   include: 'score'
+  // },
+  // callbackOnInit: null,
+  // callbackOnCreateTemplates: null
 });
+
+
+// const categoryChoices = new Choices(categorySelect, {
+//   searchEnabled: false,
+//   searchFields: ['label', 'value'],
+//   itemSelectText: '',
+  // classNames: {
+  //   containerOuter: 'categoryChoices',
+  //   containerInner: 'choices__inner',
+  //   input: 'choices__input',
+  //   inputCloned: 'choices__input--cloned',
+  //   list: 'choices__list',
+  //   listItems: 'choices__list--multiple',
+  //   listSingle: 'choices__list--single',
+  //   listDropdown: 'choices__list--dropdown',
+  //   item: 'choices__item',
+  //   itemSelectable: 'choices__item--selectable',
+  //   itemDisabled: 'choices__item--disabled',
+  //   itemChoice: 'choices__item--choice',
+  //   placeholder: 'choices__placeholder',
+  //   group: 'choices__group',
+  //   groupHeading: 'choices__heading',
+  //   button: 'choices__button',
+  //   activeState: 'is-active',
+  //   focusState: 'is-focused',
+  //   openState: 'is-open',
+  //   disabledState: 'is-disabled',
+  //   highlightedState: 'is-highlighted',
+  //   selectedState: 'is-selected',
+  //   flippedState: 'is-flipped',
+  //   loadingState: 'is-loading',
+  //   noResults: 'has-no-results',
+  //   noChoices: 'has-no-choices'
+  // },
+// });
